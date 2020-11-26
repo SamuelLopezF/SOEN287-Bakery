fb19969295705dce8ae849bb0b1d546663cd6e3f
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2020-11-26 13:06:08 -0500

    Cookie Cart
    Added functionality to all pages. Removed order page. Will do actual cart page later today or tomorrow.

---
+++ MenuItems/Chicken_Biscuit.php
@@ -0,0 +1,132 @@
+<?php require_once(__DIR__."/../PHP/Cart_Cookies/Cart_Cookie.php"); ?>
+<?php require_once(__DIR__."/../PHP/Cart_Cookies/Cookie_Hash_Functions.php"); ?>
+<!--
+	Last Edited By: Sobhan
+	Edit Date: 26-Nov-2020
+	Edit Number: 3
+	Edit Details:
+		Cart
+-->
+<!-- PHP -->
+<?php
+    if(isset($_COOKIE["cart_cookie"]))
+        $menu = cookieToHash($_COOKIE["cart_cookie"]);
+    if(isset($_POST["add_to_cart"])){
+        $menu[$_POST["add_to_cart"]] += (int) $_POST["count"];
+        hashToCookie($menu);
+    }
+?>  
+<!-- ========================= End of Important PHP ========================= -->
+
+<!DOCTYPE html>
+<html lang = "en">
+    <!-- Head -->
+    <head>
+        <title> Chicken Biscuit | Dulceria China </title>
+        <meta charset="UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+	    <meta name = "keywords" content = "dulceria china, restaurant, bakery, chicken biscuit"/>
+	    <meta name = "description" content = "Delicious chicket biscuit"/>
+	    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+        <link type = "text/css" rel = "stylesheet" href = "../Styles/template.css"/>
+        <style type = "text/css" rel = "stylesheet">
+            .menuItem img {
+                width: 50%;
+                height: 50%;
+            }
+        </style>
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Header / Nav Bar-->
+        <header>
+            <nav>
+                <table>
+                    <tr>
+                        <th> <a href = "../index.php" class = "logo"><img width="100" src="../Styles/BakeryLogo.png" alt="Dulceria China logo" class = "logo"/> </a></th>
+                        <th><a href = "../index.php"> Home </a></th>
+                        <th><a href = "../menu.php"> Menu </a></th>
+                        <!-- <th><a href = "../order.html"> Order </a></th> -->
+                        <th><a href = "../about_us.php"> About Us </a></th>
+                        <th><a href = "../contact_us.php"> Contact Us </a></th>
+                        <th><a href = "../account.php"> Account </a></th>       
+                        <th class="cart" id='cartIconTopRight'><a href="../Cart.php"><span class="qty" id= 'cartIconTopRightQuantity'> <?php if(isSet($menu)) cartSize($menu); else echo("0");?> </span><img src="../Styles/Cart.png" alt="Cart.html" width="40px" height="40px"/></a></th>  
+                    </tr>
+                </table>
+            </nav>
+        </header> <br/>
+
+        <!-- Picture -->
+        <div class = "menuItem banner">
+            <h3> Chicken Biscuit </h3>
+            <figure>
+                <img src = "../Styles/Chk bisc2.jpg" alt = "Chicken Biscuit" class = "small_brush"/>
+                <figcaption> Delicious Chicken Biscuit! </figcaption>
+            </figure>
+            <form action = "" method = "POST">
+                <table class = "menu_button">
+                    <tr>
+                        <td><label>Pieces (#)</label></td>
+                        <td><label>Total Price ($)</label></td>   
+                    </tr>
+                    <tr>
+                        <td><input type = "text" class = "input_pieces" name = "count"/></td>
+                        <td><input type = "text" class = "input_price" name = "price"/></td>
+                    </tr>
+                    <tr>
+                        <td colspan = "2"><button type = "submit" class = "add_btn" id = "chicken_biscuit" name = "add_to_cart" value = "chicken_biscuit">Add to Cart</button></td>
+                    </tr>
+                </table>
+            </form>
+            <br/>
+        </div>
+        
+        <!-- Description -->
+        <div class = "not_banner description">
+            <h4> Description </h4>
+            <p>
+                If you like fried chicken, then you'll love our chicken biscuits. They're delicious, crunchy, and everything you could 
+                ever ask for. You even get one for only $0.50!
+            </p>
+        </div>
+
+        <!-- Ingredients -->
+        <div class = "banner ingredients">
+            <h4> Ingredients </h4>
+            <p>
+                Our chicken biscuits are made with wheat flour, eggs, water, sugar, salt, maltose, oil, pork fat, red fermented bean curd,
+                sesame seeds, garlic, and onions.
+            </p><br/>
+        </div>
+        <br/>
+        
+        <!-- Footer -->
+        <footer>
+            <table>
+                <tr id = "email">
+                    <td><img src = "../Styles/email.png" alt = "Email" width = "25px" height = "25px"/></td>
+                    <td><a href = "mailto:dulceriachina.ec@gmail.com">dulceriachina.ec@gmail.com</a></td>
+                </tr>
+                <tr id = "telephone">
+                    <td><img src = "../Styles/phone.png" alt = "Phone" width = "25px" height = "25px"/></td>
+                    <td>(04) 251-0363 - (593) 997-045-024</td>
+                </tr>
+                <tr id = "facebook">
+                    <td><img src = "../Styles/facebook.png" alt = "Facebook" width = "25px" height = "25px"/></td>
+                    <td><a href = "https://www.facebook.com/dulceriachina/" target = "_blank"> facebook.com/dulceriachina</a></td>
+                </tr>
+                <tr id = "instagram">
+                    <td><img src = "../Styles/instagram.png" alt = "Instagram" width = "25px" height = "25px"/></td>
+                    <td><a href="https://www.instagram.com/dulceriachina/">@dulceriachina</a> </td>
+                </tr>
+            </table>
+            <p> ©2020 Dulceria China </p>
+        </footer>
+
+        <!-- Script(s) -->
+        <script src = "../JavaScript/SingleMenuItems.js" type = "text/JavaScript"></script>
+
+        <!-- <script src="../JavaScript/Menu.js"></script> -->
+
+    </body>
+</html>
---
+++ MenuItems/Coconut_Bun.php
@@ -0,0 +1,131 @@
+<?php require_once(__DIR__."/../PHP/Cart_Cookies/Cart_Cookie.php"); ?>
+<?php require_once(__DIR__."/../PHP/Cart_Cookies/Cookie_Hash_Functions.php"); ?>
+<!--
+	Last Edited By: Sobhan
+	Edit Date: 26-Nov-2020
+	Edit Number: 3
+	Edit Details:
+		Cart
+-->
+<!-- PHP -->
+<?php
+    if(isset($_COOKIE["cart_cookie"]))
+        $menu = cookieToHash($_COOKIE["cart_cookie"]);
+    if(isset($_POST["add_to_cart"])){
+        $menu[$_POST["add_to_cart"]] += (int) $_POST["count"];
+        hashToCookie($menu);
+    }
+?>  
+<!-- ========================= End of Important PHP ========================= -->
+
+<!DOCTYPE html>
+<html lang = "en">
+    <!-- Head -->
+    <head>
+        <title> Coconut Bun | Dulceria China </title>
+        <meta charset="UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+	    <meta name = "keywords" content = "dulceria china, restaurant, bakery, coconut bun"/>
+	    <meta name = "description" content = "Delicious coconut bun"/>
+	    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+        <link type = "text/css" rel = "stylesheet" href = "../Styles/template.css"/>
+        <style type = "text/css" rel = "stylesheet">
+            .menuItem img {
+                width: 50%;
+                height: 50%;
+            }
+        </style>
+    </head>
+    <!-- Body -->
+    <body>
+        <!-- Header / Nav Bar-->
+        <header>
+            <nav>
+                <table>
+                    <tr>
+                        <th> <a href = "../index.php" class = "logo"><img width="100" src="../Styles/BakeryLogo.png" alt="Dulceria China logo" class = "logo"/> </a></th>
+                        <th><a href = "../index.php"> Home </a></th>
+                        <th><a href = "../menu.php"> Menu </a></th>
+                        <!-- <th><a href = "../order.html"> Order </a></th> -->
+                        <th><a href = "../about_us.php"> About Us </a></th>
+                        <th><a href = "../contact_us.php"> Contact Us </a></th>
+                        <th><a href = "../account.php"> Account </a></th>       
+                        <th class="cart" id='cartIconTopRight'><a href="../Cart.php"><span class="qty" id= 'cartIconTopRightQuantity'> <?php if(isSet($menu)) cartSize($menu); else echo("0");?> </span><img src="../Styles/Cart.png" alt="Cart.html" width="40px" height="40px"/></a></th>  
+                    </tr>
+                </table>
+            </nav>
+        </header> <br/>
+
+        <!-- Picture -->
+        <div class = "menuItem banner">
+            <h3> Coconut Bun </h3>
+            <figure>
+                <img src = "../Styles/Coconut bun22.jpg" alt = "Coconut Bun" class = "small_brush"/>
+                <figcaption> Delicious Coconut Bun! </figcaption>
+            </figure>
+            <form action = "" method = "POST">
+                <table class = "menu_button">
+                    <tr>
+                        <td><label>Pieces (#)</label></td>
+                        <td><label>Total Price ($)</label></td>   
+                    </tr>
+                    <tr>
+                        <td><input type = "text" class = "input_pieces" name = "count"/></td>
+                        <td><input type = "text" class = "input_price" name = "price"/></td>
+                    </tr>
+                    <tr>
+                        <td colspan = "2"><button type = "submit" class = "add_btn" id = "coconut_bun" name = "add_to_cart" value = "coconut_bun">Add to Cart</button></td>
+                    </tr>
+                </table>
+            </form>
+            <br/>
+        </div>
+        
+        <!-- Description -->
+        <div class = "not_banner description">
+            <h4> Description </h4>
+            <p>
+                Our coconut buns are a treat to be desired. They're everything you could want from a bun and more! 
+                You can get some from our bakery for only $0.80.
+            </p>
+        </div>
+
+        <!-- Ingredients -->
+        <div class = "banner ingredients">
+            <h4> Ingredients </h4>
+            <p>
+                Our coconut buns are made with wheat flour, water, sugar, dry yeast, baking powder, vegetable oil, salt, and coconut.
+            </p><br/>
+        </div>
+        <br/>
+        
+        <!-- Footer -->
+        <footer>
+            <table>
+                <tr id = "email">
+                    <td><img src = "../Styles/email.png" alt = "Email" width = "25px" height = "25px"/></td>
+                    <td><a href = "mailto:dulceriachina.ec@gmail.com">dulceriachina.ec@gmail.com</a></td>
+                </tr>
+                <tr id = "telephone">
+                    <td><img src = "../Styles/phone.png" alt = "Phone" width = "25px" height = "25px"/></td>
+                    <td>(04) 251-0363 - (593) 997-045-024</td>
+                </tr>
+                <tr id = "facebook">
+                    <td><img src = "../Styles/facebook.png" alt = "Facebook" width = "25px" height = "25px"/></td>
+                    <td><a href = "https://www.facebook.com/dulceriachina/" target = "_blank"> facebook.com/dulceriachina</a></td>
+                </tr>
+                <tr id = "instagram">
+                    <td><img src = "../Styles/instagram.png" alt = "Instagram" width = "25px" height = "25px"/></td>
+                    <td><a href="https://www.instagram.com/dulceriachina/">@dulceriachina</a> </td>
+                </tr>
+            </table>
+            <p> ©2020 Dulceria China </p>
+        </footer>
+
+        <!-- Script(s) -->
+        <script src = "../JavaScript/SingleMenuItems.js" type = "text/JavaScript"></script>
+
+        <!-- <script src="../JavaScript/Menu.js"></script> -->
+
+    </body>
+</html>
