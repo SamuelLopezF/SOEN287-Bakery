96abfc17a2828cf6d098ab43a27bfb750d69682f
Alushx <72513274+Alushx@users.noreply.github.com>
2020-12-07 21:23:47 -0500

    Add files via upload

--- comments_functions.php
+++ comments_functions.php
@@ -11,7 +11,7 @@ date_default_timezone_set('America/Toronto');
 
             $result = $connect->query($sql);
         }
-        }
+    }
 
     function getComment($connect){
         $sql = "SELECT * FROM comments";
@@ -21,42 +21,45 @@ date_default_timezone_set('America/Toronto');
                 echo $row['username'].":<br/>";
                 echo $row['date']."<br/>";
                 echo $row['comment']."<br/><br/>";
-            echo "</p>
-            <form method='POST' action='".deleteComment($connect)."' >
-                    <input type='hidden' name='id' value='".$row['id']."'>
-                    <button type='submit' name='delete'>Delete</button>
-                </form>
-                <form method='POST' action='edit_comment.php'>
-                    <input type='hidden' name='id' value='".$row['id']."'>
-                    <input type='hidden' name='username' value='".$row['username']."'>
-                    <input type='hidden' name='date' value='".$row['date']."'>
-                    <input type='hidden' name='comment' value='".$row['comment']."'>
-                    <button>Edit</button>
-                </form></div>";
+            echo "</div>";
+
+            // echo </p>
+            // <form method='POST' action='".deleteComment($connect)."' >
+            //         <input type='hidden' name='id' value='".$row['id']."'>
+            //         <button type='submit' name='delete'>Delete</button>
+            //     </form>
+            //     <form method='POST' action='edit_comment.php'>
+            //         <input type='hidden' name='id' value='".$row['id']."'>
+            //         <input type='hidden' name='username' value='".$row['username']."'>
+            //         <input type='hidden' name='date' value='".$row['date']."'>
+            //         <input type='hidden' name='comment' value='".$row['comment']."'>
+            //         <button>Edit</button>
+            //     </form>
+                // </div>";
         }
         
     }
 
-    function editComment($connect){
-        if(isset($_POST["submit"])){
-            $id = $_POST["id"];
-            $username = $_POST["username"];
-            $date = $_POST["date"];
-            $comment = $_POST["comment"];
+    // function editComment($connect){
+    //     if(isset($_POST["submit"])){
+    //         $id = $_POST["id"];
+    //         $username = $_POST["username"];
+    //         $date = $_POST["date"];
+    //         $comment = $_POST["comment"];
 
-            $sql = "UPDATE comments SET comment='$comment' WHERE id='$id'";
-            $result = $connect->query($sql);
-            header("Location: index.php");
-        }
-    }
+    //         $sql = "UPDATE comments SET comment='$comment' WHERE id='$id'";
+    //         $result = $connect->query($sql);
+    //         header("Location: index.php");
+    //     }
+    // }
 
-    function deleteComment($connect){
-        if(isset($_POST["delete"])){
-            $id = $_POST["id"];
+    // function deleteComment($connect){
+    //     if(isset($_POST["delete"])){
+    //         $id = $_POST["id"];
 
-            $sql = "DELETE FROM comments WHERE id='$id'";
-            $result = $connect->query($sql);
-            header("Location: index.php");
-        }
-        }
+    //         $sql = "DELETE FROM comments WHERE id='$id'";
+    //         $result = $connect->query($sql);
+    //         header("Location: index.php");
+    //     }
+    // }
 ?>
