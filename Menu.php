873455a476ad2ad88140f5ad14ca5501db77496d
SamuelLopezF <43799537+SamuelLopezF@users.noreply.github.com>
2020-12-07 21:27:05 -0500

    removed comment at top

--- Menu.php
+++ Menu.php
@@ -1,15 +1,6 @@
 <?php require_once("PHP/Cart_Cookies/Cart_Cookie.php"); ?>
 <?php require_once("PHP/Cart_Cookies/Cookie_Hash_Functions.php"); ?>
 <?php session_start() ?>
-<!--
-	Last Edited By: Sobhan
-	Edit Date: 06-Dec-2020
-	Edit Number: 5
-	Edit Details:
-		Provided cart functionality directly from the menu
-        Updated nav bar
--->
-<!-- PHP -->
 <?php
     if(isset($_COOKIE["cart_cookie"]))
         $menu = cookieToHash($_COOKIE["cart_cookie"]);
